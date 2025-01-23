dbf4d864e042d9dce3b70ba7a17968f9694c44ba
Raonikhil7488 <nikhilrao7488@gmail.com>
2025-01-23 15:37:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+ const textBox = document.getElementById('evaluatedText');
+    const letterCountDisplay = document.getElementById('letterCount');
+
+    // Event listener to count letters in real-time
+    textBox.addEventListener('input', function() {
+      const letterCount = textBox.value.length; // Get the length of the input value
+      letterCountDisplay.textContent = letterCount; // Update the h3 element with the letter count
+    });
